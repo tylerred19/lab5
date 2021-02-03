b89f2c34518cf103d8c61895df718a59ae3514e9
Tam Tran <ttt016@ucsd.edu>
2021-02-03 14:52:20 -0800

    update lab5

--- routes/add.js
+++ routes/add.js
@@ -1,6 +1,9 @@
 var data = require("../data.json");
 
 exports.addFriend = function(request, response) { 
-	response.render('index', data);   
+	response.render('index', data);
 	// Your code goes here
+	var newFriend = { "name" : request.query.name, "description":  request.query.description, "imageURL": "http://lorempixel.com/400/400/people" };
+	data.friends.push(newFriend);
+	// console.log(newFriend);
  }
